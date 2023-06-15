f18bc2f16134a8d16306b8212aaf0e7cf7e01908
Aron Herba <74024354+ahlouis@users.noreply.github.com>
2023-06-15 09:50:05 -0400

    Add files via upload

--- news_data.py
+++ news_data.py
@@ -1,56 +1,52 @@
 import pandas as pd
-import subprocess
+from newsapi import NewsApiClient
 
 class NewsData:
     def __init__(self):
-        self.data = None
-
-    def set_working_directory(self, directory):
-        pass
-
-    def load_libraries(self):
-        subprocess.call('pip install remotes ggplot2 dplyr plyr newsapi', shell=True)
-        subprocess.call('pip install git+https://github.com/news-r/newsapi', shell=True)
-
-        import newsapi
-        newsapi.newsapi_key("f10e794ff2cb4a6085ba5c32b3dc36bc")
-
-        self.data = pd.DataFrame(columns=['Country'])
-
+        self.newsapi = NewsApiClient(api_key='f10e794ff2cb4a6085ba5c32b3dc36bc')
+    
     def fetch_news(self, country):
-        from newsapi import newsapi
+        everything = self.newsapi.get_everything(q=country)
+        top_headlines = self.newsapi.get_top_headlines(q=country)
+        return everything, top_headlines
+    
+    def collect_data(self):
+        countries = [
+            "China", "Canada", "Germany", "Malaysia", "Mexico",
+            "Netherlands", "Philippines", "Switzerland", "Taiwan", "United States",
+            "Austria", "Belgium", "Bosnia and Herzegovina", "Brazil", "Bulgaria",
+            "Croatia", "Czech Republic", "East Timor", "France", "India",
+            "Ireland", "Israel", "Italy", "Liechtenstein", "Monaco",
+            "Norway", "Poland", "Portugal", "Romania", "Russia",
+            "Serbia", "Slovakia", "Slovenia", "South Africa", "South Korea",
+            "Spain", "Sweden", "Turkey", "United Kingdom", "Hungary",
+            "Argentina", "Belarus", "Morocco", "Thailand", "North Macedonia",
+            "Australia", "New Zealand", "Queensland", "Hong Kong"
+        ]
+        
+        everything_data = []
+        top_headlines_data = []
+        
+        for country in countries:
+            everything, top_headlines = self.fetch_news(country)
+            
+            everything_df = pd.DataFrame(everything['articles'])
+            top_headlines_df = pd.DataFrame(top_headlines['articles'])
+            
+            everything_df['Country'] = country
+            top_headlines_df['Country'] = country
+            
+            everything_data.append(everything_df)
+            top_headlines_data.append(top_headlines_df)
+        
+        everything_data_combined = pd.concat(everything_data, ignore_index=True)
+        top_headlines_data_combined = pd.concat(top_headlines_data, ignore_index=True)
+        
+        everything_data_combined.drop_duplicates(subset='title', keep='first', inplace=True)
+        top_headlines_data_combined.drop_duplicates(subset='title', keep='first', inplace=True)
+        
+        everything_data_combined.to_csv('new_news_master.csv', index=False)
+        top_headlines_data_combined.to_csv('top_news_master.csv', index=False)
 
-        dat = newsapi.every_news(country)
-        data = newsapi.top_headlines(country)
-
-        dat['Country'] = country
-        data['Country'] = country
-
-        self.data = pd.concat([self.data, dat])
-
-    def save_data(self, filename):
-        self.data.to_csv(filename, index=False)
-
-
-if __name__ == "__main__":
-    news_data = NewsData()
-
-    news_data.load_libraries()
-
-    countries = [
-        "China", "Canada", "Germany", "Malaysia", "Mexico",
-        "Netherlands", "Philipines", "Switzerland", "Taiwan", "United States",
-        "Austria", "Belgium", "Bosnia and Herzegovina", "Brazil", "Bulgaria",
-        "Croatia", "Czech Republic", "East Timor", "France", "India",
-        "Ireland", "Israel", "Italy", "Liechtenstein", "Monaco",
-        "Norway", "Poland", "Portugal", "Romania", "Russian Federation",
-        "Serbia", "Slovakia", "Slovenia", "South Africa", "South Korea",
-        "Spain", "Sweden", "Turkey", "United Kingdom", "Hungary",
-        "Argentina", "Belarus", "Morocco", "Thailand", "North Macedonia",
-        "Australia", "New Zealand", "Queensland", "Hong Kong"
-    ]
-
-    for country in countries:
-        news_data.fetch_news(country)
-
-    news_data.save_data("new_news_master.csv")
+news_data = NewsData()
+news_data.collect_data()
